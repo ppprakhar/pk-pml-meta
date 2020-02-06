0eaf50cf03f1305e638fc1e8c0b1113683673253
ppprakhar <45271093+ppprakhar@users.noreply.github.com>
2020-02-06 21:11:40 +0530

    Update pk-pml-meta.php

--- pk-pml-meta.php
+++ pk-pml-meta.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Paid Memberships Pro LevelMeta (Addon)
-Plugin URI: #
-Description: #
+Plugin URI: https://pk-techie.blogspot.com/2020/02/paid-memberships-pro-levelmeta-addon.html
+Description: Useful to get, update, delete memership level meta data like wordpress meta data
 Author: PrakharKant
 Version: 1.0
-Author URI: #
+Author URI: https://pk-techie.blogspot.com/
 */ 
 
 error_reporting(E_ALL); 
